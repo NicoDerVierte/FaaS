937096044502db23f030ea54b15733cad8738106
Nico Wullschleger <nico.wullschleger@ict-campus.net>
2026-06-04 14:59:07 +0200

    tete

--- src/functions/getItems.js
+++ src/functions/getItems.js
@@ -8,6 +8,6 @@ app.http('getItems', {
 
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        return { body: `Hallo, ${name}!` };
     }
 });
